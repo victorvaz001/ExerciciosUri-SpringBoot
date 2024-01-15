63ed863c6c06488db2d5e3ddca1ed49d89c7c6f9
victorvaz001 <victorvaz001@gmail.com>
2024-01-15 11:32:07 -0300

    Consulta simples - Busca por status = 'RS' - Spring Boot

--- uri2602/src/main/java/com/devsuperior/uri2602/Uri2602Application.java
+++ uri2602/src/main/java/com/devsuperior/uri2602/Uri2602Application.java
@@ -1,12 +1,36 @@
 package com.devsuperior.uri2602;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Uri2602Application {
+import com.devsuperior.uri2602.dto.CustomerMinDTO;
+import com.devsuperior.uri2602.projections.CustomerMinProjection;
+import com.devsuperior.uri2602.repositories.CustomerRespository;
 
+@SpringBootApplication
+public class Uri2602Application implements CommandLineRunner {
+
+	@Autowired
+	private CustomerRespository respository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Uri2602Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		List<CustomerMinProjection> list = respository.search1("RS");
+		List<CustomerMinDTO> result1 = list.stream().map(x -> new CustomerMinDTO(x)).collect(Collectors.toList()); /*Convertendo a lista original para dto*/
+		
+		for (CustomerMinDTO obj : result1) {
+			System.out.println(obj);
+		}
+		
 	}
 }
