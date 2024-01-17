34dfcca8875efe7d3e77b271f28936d7c7e6cde7
victorvaz001 <victorvaz001@gmail.com>
2024-01-17 08:14:09 -0300

    Respository & Consulta - SQL Raiz uri2611

---
+++ uri2611/src/main/java/com/devsuperior/uri2611/respositories/MovieRepository.java
@@ -0,0 +1,18 @@
+package com.devsuperior.uri2611.respositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.devsuperior.uri2611.entities.Movie;
+import com.devsuperior.uri2611.projections.MovieMinProjection;
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+	
+	@Query(nativeQuery = true, value = "SELECT movies.id, movies.name "
+			+ "FROM movies "
+			+ "INNER JOIN genres ON movies.id_genres = genres.id "
+			+ "WHERE genres.description = :genreName")
+	List<MovieMinProjection> search1(String genreName);
+}
