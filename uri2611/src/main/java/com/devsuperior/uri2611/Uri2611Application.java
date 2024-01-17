6a6f0b4ea03114c3526329e5f417fe0cf125018e
victorvaz001 <victorvaz001@gmail.com>
2024-01-17 08:35:12 -0300

    uri2611 chamando o metodo para projeção, CommandLineRunner, na classe Application

--- uri2611/src/main/java/com/devsuperior/uri2611/Uri2611Application.java
+++ uri2611/src/main/java/com/devsuperior/uri2611/Uri2611Application.java
@@ -1,12 +1,34 @@
 package com.devsuperior.uri2611;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+import com.devsuperior.uri2611.projections.MovieMinProjection;
+import com.devsuperior.uri2611.respositories.MovieRepository;
+
 @SpringBootApplication
-public class Uri2611Application {
+public class Uri2611Application implements CommandLineRunner  {
+	
+	@Autowired
+	private MovieRepository repository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Uri2611Application.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		List<MovieMinProjection> list = repository.search1("Action");
+		
+	}
+
+
+
+
 }
