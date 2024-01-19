ceeab8190338c745c5bdf2de59c0adba43c3944d
victorvaz001 <victorvaz001@gmail.com>
2024-01-19 11:12:47 -0300

    uri2621 - SQL Raiz - Spring Boot

--- uri2621/src/main/java/com/devsuperior/uri2621/repositories/ProductRepository.java
+++ uri2621/src/main/java/com/devsuperior/uri2621/repositories/ProductRepository.java
@@ -1,9 +1,20 @@
 package com.devsuperior.uri2621.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.uri2621.entities.Product;
+import com.devsuperior.uri2621.projections.ProductMinProjection;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+	@Query(nativeQuery = true, value = "SELECT products.name "
+			+ "FROM products "
+			+ "INNER JOIN providers "
+			+ "on products.id_providers = providers.id "
+			+ "WHERE products.amount BETWEEN :min AND :max "
+			+ "AND providers.name LIKE CONCAT(:beginName, '%');")
+	List<ProductMinProjection> seartch1(Integer min, Integer max, String beginName);
 }
